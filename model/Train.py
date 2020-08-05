e3aa792ea2d267b95cc9c7c3caceb6894800fd47
deya <30326911+miscdats@users.noreply.github.com>
2020-08-04 18:36:44 -0700

    Add: Log .........cabin.

--- model/Train.py
+++ model/Train.py
@@ -4,6 +4,8 @@ from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
+import numpy as np
+
 # import joblib
 
 
@@ -14,6 +16,7 @@ PLS = [PLAYLIST_BJORK_CSV, PLAYLIST_ME_CSV]
 
 def get_songs_frame(playlists):
     """Returns dataframe of songs info from list of playlist CSVs."""
+    print('Reading in CSV files...')
     songs_df = pd.concat(map(pd.read_csv, playlists))
     songs_df.reset_index(drop=True, inplace=True)
     songs_df.drop('Unnamed: 0', 1, inplace=True)
@@ -21,13 +24,22 @@ def get_songs_frame(playlists):
 
 
 def clean(songs_df):
-    """Drop unwanted columns for modeling (id, artist, all_artists, album)."""
-    songs_df = songs_df.drop(['id', 'artist', 'all_artists', 'album'], axis=1)
-    return songs_df
+    """Normalize data for modeling by setting values and dropping columns (id, artist, all_artists, album)."""
+    print('Transforming data to be used...')
+    inspo_df = songs_df.loc[songs_df['is_bjork_inspo'] == True].copy()
+    songz_df = songs_df.loc[songs_df['is_bjork_inspo'] == False].copy()
+    inspo_df['is_bjork_inspo'] = np.ones((len(inspo_df), 1), dtype=int)
+    songz_df['is_bjork_inspo'] = np.zeros((len(songz_df), 1), dtype=int)
+    songs = inspo_df.append(songz_df, ignore_index=False)
+    songs = songs.drop_duplicates()
+    songs = songs.drop(['id', 'artist', 'all_artists', 'album'], axis=1)
+    prediction = songs.drop(['title'], axis=1)
+    return songs, prediction
 
 
 def split_x_y(songs, prediction):
     """Splits our dataframes into x / y train / test to return and displays snippet."""
+    print('Splitting train and test datas...')
     X_train = prediction.drop('is_bjork_inspo', axis=1)
     X_test = songs.drop(['is_bjork_inspo', 'title'], axis=1)
     y_train = prediction['is_bjork_inspo']
@@ -42,6 +54,7 @@ def split_x_y(songs, prediction):
 
 def train_models(X_train, X_test, y_train, y_test):
     """Trains model to return predictions with LogisticRegression and KNeighborsClassifier."""
+    print('Training our models!')
     lr_model = LogisticRegression()
     lr_model.fit(X_train, y_train)
     lr_pred = lr_model.predict(X_test)
@@ -66,13 +79,16 @@ def train_models(X_train, X_test, y_train, y_test):
 
 
 def load_dfs():
+    """Helper driver function to get in our data for modeling."""
+    print('Get our data and make it usable.')
     all_songs_df = get_songs_frame(PLS)
-    trainable = clean(all_songs_df)
-    prediction = trainable.copy().drop(['title'], axis=1)
+    trainable, prediction = clean(all_songs_df)
     return trainable, prediction
 
 
 def main():
+    """The main event. Gets a prediction: will you be as inspired as Bjork could be!?"""
+    print('START!')
     songs, prediction = load_dfs()
     X_train, X_test, y_train, y_test = split_x_y(songs, prediction)
     lr_pred = train_models(X_train, X_test, y_train, y_test)
