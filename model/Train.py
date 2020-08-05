69698aa6f83d80905ffe4d315880c8500a16b4fa
deya <30326911+miscdats@users.noreply.github.com>
2020-08-04 19:27:11 -0700

    Train.py: Add scaler for songs audio features.

--- model/Train.py
+++ model/Train.py
@@ -3,6 +3,11 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import RobustScaler
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import GridSearchCV
 import pandas as pd
 import numpy as np
 
@@ -23,6 +28,19 @@ def get_songs_frame(playlists):
     return songs_df
 
 
+def rescale(songs_df):
+    """Rescales features loaded in from spotipy API."""
+    print('Rescaling audio features...')
+    scaler = StandardScaler()
+    features = [['danceability', 'energy', 'key', 'loudness', 'speechiness',
+                'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo',
+                'duration_ms', 'time_signature', 'num_bars', 'num_sections',
+                'num_segments']]
+    for feature in features:
+        songs_df[feature] = scaler.fit_transform(songs_df[feature])
+    return songs_df
+
+
 def clean(songs_df):
     """Normalize data for modeling by setting values and dropping columns (id, artist, all_artists, album)."""
     print('Transforming data to be used...')
@@ -32,6 +50,7 @@ def clean(songs_df):
     songz_df['is_bjork_inspo'] = np.zeros((len(songz_df), 1), dtype=int)
     songs = inspo_df.append(songz_df, ignore_index=False)
     songs = songs.drop_duplicates()
+    songs = rescale(songs)
     songs = songs.drop(['id', 'artist', 'all_artists', 'album'], axis=1)
     prediction = songs.drop(['title'], axis=1)
     return songs, prediction
@@ -39,16 +58,11 @@ def clean(songs_df):
 
 def split_x_y(songs, prediction):
     """Splits our dataframes into x / y train / test to return and displays snippet."""
-    print('Splitting train and test datas...')
+    print('Splitting train and test sets...')
     X_train = prediction.drop('is_bjork_inspo', axis=1)
     X_test = songs.drop(['is_bjork_inspo', 'title'], axis=1)
     y_train = prediction['is_bjork_inspo']
     y_test = songs['is_bjork_inspo']
-
-    X_train.head(1)
-    X_test.head(1)
-    y_train.tail(1)
-    y_test.tail(1)
     return X_train, X_test, y_train, y_test
 
 
@@ -58,15 +72,25 @@ def train_models(X_train, X_test, y_train, y_test):
     lr_model = LogisticRegression()
     lr_model.fit(X_train, y_train)
     lr_pred = lr_model.predict(X_test)
+    print('LogisticRegression')
     print(confusion_matrix(y_test, lr_pred))
     print('\n')
     print(classification_report(y_test, lr_pred))
+    # Check performance using accuracy
+    print(accuracy_score(y_test, lr_pred))
+    # Check performance using roc
+    roc_auc_score(y_test, lr_pred)
     knn_model = KNeighborsClassifier()
     knn_model.fit(X_train, y_train)
     knn_pred = knn_model.predict(X_test)
+    print('KNeighborsClassifier')
     print(confusion_matrix(y_test, knn_pred))
     print('\n')
     print(classification_report(y_test, knn_pred))
+    # Check performance using accuracy
+    print(accuracy_score(y_test, knn_pred))
+    # Check performance using roc
+    roc_auc_score(y_test, knn_pred)
     return lr_pred
     # x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=0)
     # dt = DecisionTreeClassifier().fit(x_train, y_train)
@@ -95,5 +119,5 @@ def main():
     songs['prediction'] = lr_pred
     songs.sort_values('title').head()
     final_prediction = songs[['title', 'is_bjork_inspo', 'prediction']]
-    print(final_prediction)
+    # print(final_prediction)
     # return final_prediction
