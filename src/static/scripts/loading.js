125c64c5d8c2cf4e681aad9ff25dfe057f5aabf8
deya <30326911+miscdats@users.noreply.github.com>
2020-09-01 12:41:18 -0700

    Remembered how to strings in js lolol.

--- src/static/scripts/loading.js
+++ src/static/scripts/loading.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
     var refresh_id = setInterval(function() {
         $.get(
-          "{{ url_for('process_status', job_id=appConfig.job_id) }}",
+          "{{ url_for('process_status') }}" + appConfig.job_id,
           function(data) {
             console.log('Data: ', data);
             if (data.finished == "true") {
-                let job_id = data.data.job_id;
-                window.location.replace("{{ url_for('results', job_id=job_id) }}");
+                window.location.replace("{{ url_for('results') }}" + data.data.job_id);
             }
           }
         )}
