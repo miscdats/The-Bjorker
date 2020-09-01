a94b27fa5469ba4aae75d38bcbd86a4fb67aeccd
deya <30326911+miscdats@users.noreply.github.com>
2020-09-01 12:29:55 -0700

    Change loading.js

--- src/static/scripts/loading.js
+++ src/static/scripts/loading.js
@@ -3,9 +3,10 @@ $(document).ready(function() {
         $.get(
           "{{ url_for('process_status', job_id=appConfig.job_id) }}",
           function(data) {
-            console.log(data);
+            console.log('Data: ', data);
             if (data.finished == "true") {
-              window.location.replace("{{ url_for('results', job_id=appConfig.job_id) }}");
+                let job_id = data.data.job_id;
+                window.location.replace("{{ url_for('results', job_id=job_id) }}");
             }
           }
         )}
